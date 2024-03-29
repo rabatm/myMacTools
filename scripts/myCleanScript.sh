a9ecdc2d661abb44fa57ae39de2a1f4d2ad785cb
RABAT Martin <martin.Rabat@gmail.Com>
2024-03-29 16:09:00 +0100

    creation du script clean Download, and clean cache

---
+++ scripts/myCleanScript.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+echo 'Empty the Trash on all mounted volumes and the main HDD...'
+sudo rm -rfv /Volumes/*/.Trashes/* &>/dev/null
+sudo rm -rfv ~/.Trash/* &>/dev/null
+
+echo 'Clear System Log Files...'
+sudo rm -rfv /private/var/log/asl/*.asl &>/dev/null
+sudo rm -rfv /Library/Logs/DiagnosticReports/* &>/dev/null
+sudo rm -rfv /Library/Logs/Adobe/* &>/dev/null
+rm -rfv ~/Library/Containers/com.apple.mail/Data/Library/Logs/Mail/* &>/dev/null
+rm -rfv ~/Library/Logs/CoreSimulator/* &>/dev/null
+sudo rm -rf /Library/Caches/*
+sudo rm -rf ~/Library/Caches/*
+
+echo 'Clear Adobe Cache Files...'
+sudo rm -rfv ~/Library/Application\ Support/Adobe/Common/Media\ Cache\ Files/* &>/dev/null
+
+echo 'Cleanup iOS Applications...'
+rm -rfv ~/Music/iTunes/iTunes\ Media/Mobile\ Applications/* &>/dev/null
+
+echo 'Remove iOS Device Backups...'
+rm -rfv ~/Library/Application\ Support/MobileSync/Backup/* &>/dev/null
+
+echo 'Cleanup XCode Derived Data and Archives...'
+rm -rfv ~/Library/Developer/Xcode/DerivedData/* &>/dev/null
+rm -rfv ~/Library/Developer/Xcode/Archives/* &>/dev/null
+
+echo 'Cleanup pip cache...'
+rm -rfv ~/Library/Caches/pip
+
+echo 'Vider les fichiers temporaires'
+sudo rm -rf /private/var/folders/*/*/*/com.apple.LaunchServices*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.iconservices*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.notificationcenter*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.systempreferences*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.universalaccess*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.appstore*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.Safari*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.iTunes*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.mail*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.Maps*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.iBooks*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.PhotoBooth*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.iPhoto*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.iMovie*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.Keynote*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.Numbers*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.Pages*
+sudo rm -rf /private/var/folders/*/*/*/com.apple.iWork*
+
+
+if type "brew" &>/dev/null; then
+    echo 'Update Homebrew Recipes...'
+    brew update
+    echo 'Upgrade and remove outdated formulae'
+    brew upgrade
+    echo 'Cleanup Homebrew Cache...'
+    brew cleanup -s &>/dev/null
+    #brew cask cleanup &>/dev/null
+    rm -rfv $(brew --cache) &>/dev/null
+    brew tap --repair &>/dev/null
+fi
+
+if [ "$PYENV_VIRTUALENV_CACHE_PATH" ]; then
+    echo 'Removing Pyenv-VirtualEnv Cache...'
+    rm -rfv $PYENV_VIRTUALENV_CACHE_PATH &>/dev/null
+fi
+
+if type "npm" &> /dev/null; then
+    echo 'Cleanup npm cache...'
+    npm cache clean --force
+fi
+
+if type "yarn" &> /dev/null; then
+    echo 'Cleanup Yarn Cache...'
+    yarn cache clean --force
+fi
+
+echo 'Purge inactive memory...'
+sudo purge
+
+echo 'Success!'
---
+++ scripts/myDownloadClean.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Définir le chemin du dossier "Téléchargements"
+downloads_dir="$HOME/Downloads"
+
+# Adresse e-mail de destination
+email_dest="martin.dev@ik.me"
+
+# Vérifier si le dossier existe
+if [ -d "$downloads_dir" ]; then
+    # Envoyer un e-mail avant de supprimer les fichiers
+    echo "Le script va supprimer tous les éléments du dossier 'Téléchargements'." | mail -s "Suppression des téléchargements" "$email_dest"
+
+    spleep 10
+    # Supprimer tous les fichiers et dossiers dans le dossier "Téléchargements"
+    rm -rf "$downloads_dir"/*
+    echo "Tous les éléments du dossier 'Téléchargements' ont été supprimés."
+else
+    echo "Le dossier 'Téléchargements' n'existe pas."
+fi
